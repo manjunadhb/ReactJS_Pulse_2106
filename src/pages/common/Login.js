97db5c6124e0c2dc90f00a842aa00a50ac63d295
Dhirpsb <90253380+Dhirpsb@users.noreply.github.com>
2021-09-23 16:07:20 +0530

    done

--- src/pages/common/Login.js
+++ src/pages/common/Login.js
@@ -4,6 +4,9 @@ export default function Login() {
     return (
         <div>
             <h1>Login</h1>
+            <input type="text"/>
+            <input type="password"/>
+            <button>LOGIn</button>
         </div>
     )
 }
