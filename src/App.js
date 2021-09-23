415bc6e32ed79177095e5d39ce20bbfc15dc1aa1
manjunadhb <manjunadhb@gmail.com>
2021-09-23 15:27:08 +0530

    added pavani and shashi h3 elements.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Shashi <code>src/App.js</code> and save to reload.
-=======
-          Pavani <code>src/App.js</code> modified by Pvni.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Pavani is done with clone and push</h3>
+        <h3>Shashi Bushan is done with clone and push</h3>
       </header>
     </div>
   );
