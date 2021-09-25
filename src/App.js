1c85fc458c184cbf03e0dcf4a54832ec6bbabbbb
Dhirpsb <90253380+Dhirpsb@users.noreply.github.com>
2021-09-25 11:10:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Saleem <code>src/App.js</code> modified by akbar.
@@ -18,12 +17,11 @@ function App() {
         >
           Learn React
         </a>
-=======
+
       <h3>Pavani is done with clone and push</h3>
         <h3>Shashi Bushan is done with clone and push</h3>
         <h3>Sushma is done with clone and push</h3>
         <h3>Sachin Netha is done with clone and push</h3>
->>>>>>> 475adaa86604489f61e9ec79e3e848162f8d1e6d
       </header>
     </div>
   );
--- src/pages/common/Login.js
+++ src/pages/common/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+
+    const [registeredEmail, setregisteredEmail] = useState("")
+    const [registeredPassword, setregisteredPassword] = useState("")
+
+    let sendData = new FormData();
+    sendData.append(registeredEmail)
+    sendData
+
+
+    const getData = ()=>{
+        const response = fetch("https://pulse.brninfotech.com/pulse/modules/admin/ValidateLogin.php")
+    }
     return (
         <div>
             <h1>Login</h1>
             <input type="text"/>
             <input type="password"/>
-            <button>LOGIN</button>
+            <button onClick={getData}>LOGIN</button>
         </div>
     )
 }
